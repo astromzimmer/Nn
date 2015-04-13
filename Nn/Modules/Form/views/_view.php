819e7632073a9407a92b7123b15153d53a5c61bd
Anthon Astrom <anthon@astromzimmer.com>
2015-04-13 20:39:51 +0200

    Removing app specific stuff, and and fixing proken form

--- Nn/Modules/Form/views/_view.php
+++ Nn/Modules/Form/views/_view.php
@@ -1,4 +1,10 @@
-<form method="POST" action="<?php echo DOMAIN.DS.'forms'.DS.'submit'.DS.$form->attr('id') ?>" class="<?php if($form->attr('id')) echo $form->attribute()->attributetype()->cleanName() ?>">
+<form id="form-<?php echo $form->attr('id') ?>" method="POST" action="<?php echo DOMAIN.DS.'forms'.DS.'submit'.DS.$form->attr('id') ?>" class="<?php if($form->attr('id')) echo $form->attribute()->attributetype()->cleanName() ?>">
 	<input name="spmchk" type="text" id="spmchk" value="hmn" style="display: none;" />
 	<div id="form<?php if($form->attr('id')) echo htmlentities($form->attr('id')); ?>-content" data-id="<?php if($form->attr('id')) echo $form->attr('id') ?>"><?php if($form->attr('id')) echo $form->content(); ?></div>
 </form>
+<script type="text/javascript">
+	var callback_path = document.location,
+		form = document.getElementById('form-<?php echo $form->attr('id') ?>'),
+		action = form.getAttribute('action');
+	form.setAttribute('action',action+'?callback='+callback_path);
+</script>
