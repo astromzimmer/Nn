29003c9b2d2c6109c1b6d52b3b8545b79747fd07
Anthon Astrom <anthon@astromzimmer.com>
2015-04-15 23:19:57 +0200

    Fixing default image view

--- Nn/Modules/Image/views/_view.php
+++ Nn/Modules/Image/views/_view.php
@@ -1,12 +1,6 @@
 <?php if($image->hasFile()): ?>
 	<?php $size = $image->size(); ?>
-	<div class="image" data-id="<?php echo $image->attr('id') ?>"><img alt="<?php echo $image->title() ?>"
-	src="<?php echo $image->src() ?>"
-	data-bw_thumb_src="<?php echo $image->src(220,false,true) ?>"
-	data-thumb_src="<?php echo $image->src(220) ?>"
-	data-full_src="<?php echo $image->src() ?>"
-	width="<?php $size[0] ?>"
-	height="<?php $size[1] ?>" /></div>
+	<div class="image" data-id="<?php echo $image->attr('id') ?>"><?php echo $image->tag(); ?></div>
 <?php else: ?>
 	No image file found.
 <?php endif; ?>
