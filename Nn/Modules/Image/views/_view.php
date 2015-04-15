ff515fd3e90ef439c18630838bfd1cf9acb29c85
Anthon Astrom <anthon@astromzimmer.com>
2015-04-15 23:20:52 +0200

    Fixing default image view

--- Nn/Modules/Image/views/_view.php
+++ Nn/Modules/Image/views/_view.php
@@ -1,5 +1,4 @@
 <?php if($image->hasFile()): ?>
-	<?php $size = $image->size(); ?>
 	<div class="image" data-id="<?php echo $image->attr('id') ?>"><?php echo $image->tag(); ?></div>
 <?php else: ?>
 	No image file found.
