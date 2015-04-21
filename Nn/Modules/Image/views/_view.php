119152709f79866975de4dc66195ebe258a32059
Anthon Astrom <anthon@astromzimmer.com>
2015-04-21 12:43:37 +0200

    Factoring out Document to Attachment+Document, and adding image links

--- Nn/Modules/Image/views/_view.php
+++ Nn/Modules/Image/views/_view.php
@@ -1,5 +1,11 @@
 <?php if($image->hasFile()): ?>
-	<div class="image" data-id="<?php echo $image->attr('id') ?>"><?php echo $image->tag(920); ?></div>
+	<div class="image" data-id="<?php echo $image->attr('id') ?>">
+	<?php if($image->hasHref()): ?>
+		<a href="<?php echo $image->attr('href') ?>"><?php echo $image->tag(920); ?></a>
+	<?php else: ?>
+		<?php echo $image->tag(920); ?>
+	<?php endif; ?>
+	</div>
 <?php else: ?>
 	No image file found.
 <?php endif; ?>
