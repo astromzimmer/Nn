060bb5f740e7ec83c217d3bf5e67a03c8c2e2f0f
Anthon Astrom <anthon@astromzimmer.com>
2015-06-08 16:01:56 +0200

    Adding EN

---
+++ Nn/Babel/EN.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	
+	'payment_address_title' => 'Address update',
+	'payment_address_info' => 'Before you make your payment, please have a look through your contact information, to make sure everything is up to date.',
+	'payment_pay_title' => 'Payment form',
+	'payment_pay_info' => 'This transaction is handled by <a href="http://stripe.com">Stripe</a>, and this form is submitted via an encryped request in the background. No credit card details are saved on our servers.',
+	'payment_done_info'	=> 'Payment successful. A confirmation has been sent to your email address.',
+
+	'payment_confirmation_email_subject' => 'kleio - Subscription payment successfull',
+	'payment_confirmation_email_html' => '<h4>Payment successful!</h4><br>Your <a href="http://kleio.com">kleio</a> subscription for the next year is paid up.<br><br>It\'s great to see that you\'ll stay with kleio, and we hope you\'ll continue enjoying it as much as we are!<br><br><br>/ the kleio team',
+	'payment_confirmation_email_text' => "Payment successful!\r\n\r\n\r\nYour kleio subscription for the next year is paid up.\r\n\r\nIt's great to see that you'll stay with kleio, and we hope you'll continue enjoying it as much as we are!\r\n\r\n\r\n/ the kleio team",
+
+]
+
+?>
