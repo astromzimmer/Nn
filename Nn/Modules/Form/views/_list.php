e895da3482567e59d4a4ae7cabe46cebe3c3e1c6
Anthon Astrom <anthon@astromzimmer.com>
2016-05-21 14:52:48 +0200

    Fixed form layout

--- Nn/Modules/Form/views/_list.php
+++ Nn/Modules/Form/views/_list.php
@@ -4,11 +4,13 @@
 		<ul id="0_forms" class="sortable">
 		<?php foreach($forms as $frm): ?>
 			<li class="form<?php if(isset($form) && $frm == $form) echo ' focus' ?>" id="form_<?php echo $frm->attr('id') ?>">
-				<div class="label">
-					<a href="<?php echo DOMAIN.DS.'admin'.DS.'forms'.DS.'view'.DS.$frm->attr('id') ?>">
-						<span class="fa <?php echo $frm->attr('icon') ?>"></span>
-						<?php echo $frm->attr('name'); ?>
-					</a>
+				<div class="grouper">
+					<div class="label">
+						<a href="<?php echo DOMAIN.DS.'admin'.DS.'forms'.DS.'view'.DS.$frm->attr('id') ?>">
+							<span class="fa <?php echo $frm->attr('icon') ?>"></span>
+							<?php echo $frm->attr('name'); ?>
+						</a>
+					</div>
 				</div>
 			</li>
 		<?php endforeach ?>
