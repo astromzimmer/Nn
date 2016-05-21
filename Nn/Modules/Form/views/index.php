d34cc2f9a8d438dad87215e9c51f06b9a0509216
Anthon Astrom <anthon@astromzimmer.com>
2016-05-21 14:51:29 +0200

    Fixed form layout

--- Nn/Modules/Form/views/index.php
+++ Nn/Modules/Form/views/index.php
@@ -1,7 +1,7 @@
 <div id="left">
 	<?php Nn::partial('Form','_list',array('forms'=>$forms)) ?>
 </div>
-<div id="right">
+<div id="index">
 	<?php Nn::partial('Admin','_logo') ?>
 	<?php Nn::partial('Admin','_stats') ?>
 </div>
